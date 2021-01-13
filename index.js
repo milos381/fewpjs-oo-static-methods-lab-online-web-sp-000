6760633efb4ddddea7d5ee20e6bed0d75e5c3e5c
milos381 <milos_popovic@outlook.com>
2021-01-13 14:30:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+    return capitalizedString
+  }
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+
+  // NE RAZUMEM OVU FUNKCIJU!!!!!
+  // static titleize( sentence ) {
+  //   let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+  //   let result = [];
+  //   let arrayOfWords = sentence.split( " " )
+  //   for ( let n = 0; n < arrayOfWords.length; n++ ) {
+  //     if ( n == 0 ) {
+  //       result.push( this.capitalize( arrayOfWords[ n ] ) )
+  //     } else {
+  //       if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+  //         result.push( arrayOfWords[ n ] )
+  //       } else {
+  //         result.push( this.capitalize( arrayOfWords[ n ] ) )
+  //       }
+  //     }
+
+  //   }
+  //   return result.join( " " );
+  // }
+
+
+  static titleize(string){
+    const exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    const splittedString = string.split(" ")
+
+    const finalArray = splittedString.map(function (word) {
+        if (exceptions.includes(word)) {
+          return word
+        } else {
+          return Formatter.capitalize(word)
+        }
+    })
+    finalArray[0] = this.capitalize(finalArray[0])
+    return finalArray.join(" ")
+
+  }
+
+  static pluralize(string){
+    const wordsForChanging = ['There', 'Where', 'Here']
+    const splittedString = string.split(" ")
+    const changeFunction = splittedString.map(function (word, index, sentence) {
+      if (wordsForChanging.includes(word)) {
+        if (sentence[index + 1] === "is") {
+          return word + "'s"
+        }
+      } else {
+        return word
+      }
+    })
+    return changeFunction.join(" ")
+  }
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -42,4 +42,16 @@ describe( "Formatter", () => {
       expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
     } )
   } )
+
+  describe( "pluralize", () => {
+    it( "is a static method", () => {
+      expect( Formatter.pluralize( "There is a lot of data here" ) ).to.not.throw
+    } )
+
+    it( "pluralizes all relevant words in a sentence e.g 'there is', 'where is', 'here is'", () => {
+      expect( Formatter.pluralize( "There is a lot of data here" ) ).to.equal( "There's a lot of data here" )
+      expect( Formatter.pluralize( "Where is my pencil" ) ).to.equal( "Where's my pencil" )
+      expect( Formatter.pluralize( "Here is your pencil" ) ).to.equal( "Here's your pencil" )
+    } )
+  } )
 } )
